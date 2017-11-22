565bf0c22ae53140646813be1c64ca3205ef2723
Burlaka Dmytro <dimasion2010@gmail.com>
2017-11-22 14:54:17 +0200

    Fixes

--- app/templates/src/shared/_scripts/main.es6.js
+++ app/templates/src/shared/_scripts/main.es6.js
@@ -1,10 +1,11 @@
 // Main javascript entry point
 // Should handle bootstrapping/starting application
-import { Link } from '../_modules/Atoms/link/link';
+import { Link } from '../_modules/atoms/link/link';
 
 // ----------------------------
 // Initialization
 // ----------------------------
 let page = $('body').data('page');
+svg4everybody();
 
 page === 'main' && Link.init();
